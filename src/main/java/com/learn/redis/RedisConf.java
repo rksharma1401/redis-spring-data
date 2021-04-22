c34a9a4ed5f699aeb409eaee87ddb8dff904e0b1
Ravikant Sharma <rksharma1401@hotmail.com>
2021-04-22 16:50:43 +0530

    ADD letuce

--- src/main/java/com/learn/redis/RedisConf.java
+++ src/main/java/com/learn/redis/RedisConf.java
@@ -1,38 +1,47 @@
 package com.learn.redis;
 
 import java.time.Duration;
+
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.stereotype.Component;
-import redis.clients.jedis.JedisPoolConfig;
+
 @Component
 public class RedisConf {
 	@Bean
-	JedisConnectionFactory jedisConnectionFactory(@Value("${redis.hostname}") String host,@Value("${redis.port}") Integer port,@Value("${redis.password}") String  password ) {
-		 RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-		  redisStandaloneConfiguration.setHostName(host);
-		  redisStandaloneConfiguration.setPort(port);
-		  redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
-		  JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
-		  jedisClientConfiguration.connectTimeout(Duration.ofMillis(20000));
-		 GenericObjectPoolConfig<JedisPoolConfig> jPoolConfig=new GenericObjectPoolConfig<JedisPoolConfig>();
-		  jPoolConfig.setMaxTotal(2000);
-		  jPoolConfig.setMinIdle(20); 
-		  jedisClientConfiguration.usePooling().poolConfig(jPoolConfig); 
-		  return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration.build());
+	LettuceConnectionFactory lettuceConnectionFactory(@Value("${redis.hostname}") String host,
+			@Value("${redis.port}") Integer port, @Value("${redis.password}") String password) {
+		
+ 		 		
+		
+
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(host);
+		redisStandaloneConfiguration.setPort(port);
+		redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+		
+
+	    GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+	    poolConfig.setMaxIdle(50);
+	    poolConfig.setMinIdle(30);
+	    poolConfig.setMaxWaitMillis(3000);
+	    poolConfig.setMaxTotal(200);
+
+	    LettucePoolingClientConfiguration lettucePoolingClientConfiguration = LettucePoolingClientConfiguration.builder()
+	            .commandTimeout(Duration.ofSeconds(10))
+	            .shutdownTimeout(Duration.ZERO)
+	            .poolConfig(poolConfig)
+	            .build();
+
+	    LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration, lettucePoolingClientConfiguration);
+	    lettuceConnectionFactory.setShareNativeConnection(false);
+
+	    return lettuceConnectionFactory;
 	}
 
-	@Bean
-	public RedisTemplate<String, Object> redisTemplate(@Autowired JedisConnectionFactory connectionFactory) {
-	    RedisTemplate<String, Object> template = new RedisTemplate<>();
-	    template.setConnectionFactory(connectionFactory);
-	    return template;
-	}
 }
